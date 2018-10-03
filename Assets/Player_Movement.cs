3297d598a701234e330fa259d4373479f0f5cbf5
Danny Pantuso <dpantuso@stanford.edu>
2018-10-02 22:00:10 -0500

    Hi

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+}
